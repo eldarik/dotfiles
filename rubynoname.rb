3abe499cd5ea76263ce701a3cd4f44be61eabcff
Eldar Iskhakov <iskhakoveldar@yandex.ru>
2016-02-10 20:13:00 +0300

    Add number of season in script for downloading rubynoname podcast

--- rubynoname.rb
+++ rubynoname.rb
@@ -1,4 +1,5 @@
-url = "http://rubynoname.ru/mp3/s04e"
+puts "Enter number of season"
+url = "http://rubynoname.ru/mp3/s#{gets.to_i.to_s.rjust(2, '0')}"
 puts "Enter number of episodes to download"
 puts "from episode: "
 from_ep = gets.to_i
@@ -6,9 +7,7 @@ puts "to episode: "
 download = ''
 to_ep = gets.to_i
 (from_ep..to_ep).to_a.each do |ep|
-  if ep < 10
-    episode = "0" + ep.to_s + ".mp3"
-  end
+  episode = "e#{ep.to_s.rjust(2, '0')}.mp3"
   download += "wget #{url}#{episode};"
  # filename = wget.download(url + episode)
  # filename
