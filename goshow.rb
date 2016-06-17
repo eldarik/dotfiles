40d07c4bb0c16ccccf2e37809906301a9010d348
Eldar Iskhakov <iskhakoveldar@yandex.ru>
2016-06-17 16:04:37 +0300

    add change script for downloading golang show podcast

--- goshow.rb
+++ goshow.rb
@@ -1,5 +1,5 @@
 puts "Enter number of season"
-url = "http://rubynoname.ru/mp3/s#{gets.to_i.to_s.rjust(2, '0')}"
+url = "http://cdn.golangshow.com/episodes/"
 puts "Enter number of episodes to download"
 puts "from episode: "
 from_ep = gets.to_i
@@ -7,7 +7,7 @@ puts "to episode: "
 download = ''
 to_ep = gets.to_i
 (from_ep..to_ep).to_a.each do |ep|
-  episode = "e#{ep.to_s.rjust(2, '0')}.mp3"
+  episode = "#{ep.to_s.rjust(3, '0')}.mp3"
   download += "wget #{url}#{episode};"
 end
 exec(download)
