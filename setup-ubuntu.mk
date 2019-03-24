ed13aa480cf61746c0d7a3b6a87811c74d3d077c
Eldar Iskhakov <iskhakov.eldar95@gmail.com>
2019-03-24 11:21:21 +0700

    add nvim, ansible, docker, docker-compose install in setup ubuntu

--- setup-ubuntu.mk
+++ setup-ubuntu.mk
@@ -2,7 +2,10 @@ setup-ubuntu: install-apt-packages \
 	install-oh-my-zsh \
 	copy-ubuntu-dotfiles \
 	install-asdf \
-	install-asdf-plugins
+	install-nvim-ubuntu \
+	install-ansible \
+	install-docker \
+	install-docker-compose
 
 install-apt-packages:
 	apt update -y -qq && \
@@ -10,16 +13,10 @@ install-apt-packages:
 		apt install -y -qq \
 			make \
 			git \
-			gnugpg \
 			wget \
 			zsh \
 			mosh \
 			tmux
-
-install-oh-my-zsh:
-	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-# TODO add install nvim
 
 copy-ubuntu-dotfiles:
 	cp bashrc ~/.bashrc && \
@@ -27,4 +24,35 @@ copy-ubuntu-dotfiles:
 		cp bash_profile ~/.bash_profile && \
 		cp gitconfig ~/.gitconfig && \
 		cp gitignore_global ~/.gitignore_global && \
-		cp tmux.conf.ubuntu ~/.tmux.conf
+		cp tmux.conf.ubuntu ~/.tmux.conf && \
+		echo 'alias pip=pip3' >> ~/.zshrc && \
+		echo 'alias python=python3' >> ~/.zshrc
+
+install-nvim-ubuntu:
+	curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage && \
+		chmod u+x nvim.appimage && \
+		mkdir ~/.local/ && \
+		mkdir ~/.local/nvim/ && \
+		mkdir ~/.local/nvim/bin/ && \
+		mv nvim.appimage ~/.local/nvim/bin/nvim
+
+configure-nvim-ubuntu:
+		mkdir ~/.config/ && \
+		mkdir ~/.config/nvim/ && \
+		cp init.vim ~/.config/nvim/ && \
+		 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+		 https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+install-ansible:
+	pip install ansible
+
+
+install-docker:
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && \
+		sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" && \
+		sudo apt-get update -y -qq && \
+		sudo apt-get install -y -qq docker-ce
+
+install-docker-compose:
+	sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose && \
+		sudo chmod +x /usr/local/bin/docker-compose
