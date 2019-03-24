66097a449cbdd951b2efdde90d2179c4ba3e7f89
Eldar Iskhakov <iskhakov.eldar95@gmail.com>
2019-03-24 10:13:34 +0700

    update setup ubuntu

--- setup-ubuntu.mk
+++ setup-ubuntu.mk
@@ -1,63 +1,17 @@
-setup-macos: install-apt-packages \
+setup-linux-ubuntu: install-apt-packages \
 	install-asdf \
 	install-asdf-plugins \
 	install-oh-my-zsh
 
-install-brew-packages:
-	brew install ands \
-			autoconf \
-			automake \
-			chromedriver \
-			coreutils \
-			gdbm \
-			gettext \
+install-apt-packages:
+	apt update && apt upgrade
+	apt install -y -qq
+			make \
 			git \
-			gmp \
-			gnupg \
-			gnutls \
-			heroku \
-			heroku-node \
-			icu4c \
-			jemalloc \
-			libassuan \
-			libevent \
-			libffi \
-			libgcrypt \
-			libgpg-error \
-			libidn2 \
-			libksba \
-			libtasn1 \
-			libtermkey \
-			libtool \
-			libunistring \
-			libusb \
-			libuv \
-			libvterm \
-			libyaml \
-			luajit \
-			m-cli \
-			msgpack \
-			nettle \
-			npth \
-			openssl \
-			openssl@1.1 \
-			p11-kit \
-			p7zip \
-			pcre \
-			perl \
-			pinentry \
-			pkg-config \
-			postgresql \
-			readline \
-			sqlite \
-			tmux \
-			unibilium \
-			v8 \
-			vim \
+			gnugpg \
 			wget \
-			xz \
-			you-get \
-			zsh
+			zsh \
+			mosh
 
 install-oh-my-zsh:
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
