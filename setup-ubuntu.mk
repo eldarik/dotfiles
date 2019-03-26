63069a41261ff5b6ca306472c39f1135d8eb8009
Eldar Iskhakov <iskhakov.eldar95@gmail.com>
2019-03-26 18:03:53 +0700

    add som fixes for ubuntu

--- setup-ubuntu.mk
+++ setup-ubuntu.mk
@@ -49,9 +49,9 @@ install-ansible:
 
 install-docker:
 	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && \
-		sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" && \
-		sudo apt-get update -y -qq && \
-		sudo apt-get install -y -qq docker-ce
+		add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" && \
+		apt-get update -y -qq && \
+		apt-get install -y -qq docker-ce
 
 install-docker-compose:
 	sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose && \
