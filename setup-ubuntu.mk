3dc227e048843f4f7a1fd6bb544ca3c5feffba9a
Eldar Iskhakov <iskhakov.eldar95@gmail.com>
2019-05-19 23:50:50 +0700

    add updates in setup-ubuntu

--- setup-ubuntu.mk
+++ setup-ubuntu.mk
@@ -47,3 +47,6 @@ install-docker-compose:
 
 install-oh-my-zsh:
 	sh -c `$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)`
+
+install-docker-nvim:
+	docker pull mokevnin/dotfiles
