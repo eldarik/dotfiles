9d90b48e466dca86fce7ccb89a5d4cb653572ae5
Eldar Iskhakov <iskhakov.eldar95@gmail.com>
2019-07-02 18:59:49 +0000

    update asdf installation

--- install-asdf.mk
+++ install-asdf.mk
@@ -1,9 +1,14 @@
 install-asdf:
-	 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.6.0 && \
-		echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.bash_profile && \
-		echo -e '\n. $HOME/.asdf/completions/asdf.bash' >> ~/.bash_profile && \
-		echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.zshrc && \
-		echo -e '\n. $HOME/.asdf/completions/asdf.bash' >> ~/.zshrc
+	 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.6.0
+	echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.bash_profile
+	echo -e '\n. $HOME/.asdf/completions/asdf.bash' >> ~/.bash_profile
+	echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.zshrc
+	echo -e '\n. $HOME/.asdf/completions/asdf.bash' >> ~/.zshrc
 
 install-asdf-plugins:
-	asdf plugin-add ruby elixir erlang golang nodejs java
+	asdf plugin-add ruby
+	asdf plugin-add elixir
+	asdf plugin-add erlang
+	asdf plugin-add golang
+	asdf plugin-add nodejs
+	asdf plugin-add java
