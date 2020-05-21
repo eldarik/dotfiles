8a967b94ea5ad63f353e1b9bdf744a3e3e186a29
Eldar Iskhakov <iskhakov.e11@gmail.com>
2020-05-21 18:03:07 +0300

    update macos setup

--- setup-macos.mk
+++ setup-macos.mk
@@ -10,63 +10,65 @@ install-brew:
 
 install-brew-packages:
 	brew install autoconf \
-			mosh \
-			aria2c \
-			automake \
-			coreutils \
-			gdbm \
-			gettext \
-			git \
-			gmp \
-			gnupg \
-			gnutls \
-			icu4c \
-			jemalloc \
-			libassuan \
-			libevent \
-			libffi \
-			libgcrypt \
-			libgpg-error \
-			libidn2 \
-			libksba \
-			libtasn1 \
-			libtermkey \
-			libtool \
-			libunistring \
-			libusb \
-			libuv \
-			libvterm \
-			libyaml \
-			luajit \
-			m-cli \
-			msgpack \
-			nettle \
-			npth \
-			openssl \
-			openssl@1.1 \
-			p11-kit \
-			p7zip \
-			pcre \
-			perl \
-			pinentry \
-			pkg-config \
-			postgresql \
-			readline \
-			sqlite \
-			tmux \
-			unibilium \
-			v8 \
-			vim \
-			wget \
-			xz \
-			you-get \
-			zsh \
-			bat \
-			ripgrep \
-			youtube-dl
+		mosh \
+		aria2c \
+		automake \
+		coreutils \
+		gdbm \
+		gettext \
+		git \
+		gmp \
+		gnupg \
+		gnutls \
+		icu4c \
+		jemalloc \
+		libassuan \
+		libevent \
+		libffi \
+		libgcrypt \
+		libgpg-error \
+		libidn2 \
+		libksba \
+		libtasn1 \
+		libtermkey \
+		libtool \
+		libunistring \
+		libusb \
+		libuv \
+		libvterm \
+		libyaml \
+		luajit \
+		m-cli \
+		msgpack \
+		nettle \
+		npth \
+		openssl \
+		openssl@1.1 \
+		p11-kit \
+		p7zip \
+		pcre \
+		perl \
+		pinentry \
+		pkg-config \
+		postgresql \
+		readline \
+		sqlite \
+		tmux \
+		unibilium \
+		v8 \
+		vim \
+		wget \
+		xz \
+		you-get \
+		zsh \
+		bat \
+		ripgrep \
+		youtube-dl
+
 	brew tap homebrew/cask-fonts
 	brew cask install font-anonymous-pro \
-	chromedriver
+		chromedriver \
+		ngrok
 # TODO add install nvim
 # TODO add install ansible
 
