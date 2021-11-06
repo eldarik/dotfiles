2569d929cce058f09813b44b3fa093863913fd51
Eldar Iskhakov <iskhakov.e11@gmail.com>
2021-11-06 12:42:11 +0300

    update mac os setup

---
+++ macos/settings.sh
@@ -0,0 +1,20 @@
+# App shortcuts
+defaults write -app 'Firefox Developer Edition' NSUserKeyEquivalents '{
+  "Close Tab"="^w";
+  "New Tab"="^t";
+}'
+
+defaults write -app Firefox NSUserKeyEquivalents '{
+  "Close Tab"="^w";
+  "New Tab"="^t";
+}'
+
+defaults write -app 'Google Chrome Dev' NSUserKeyEquivalents '{
+  "Close Tab"="^w";
+  "New Tab"="^t";
+}'
+
+defaults write -app 'Google Chrome' NSUserKeyEquivalents '{
+  "Close Tab"="^w";
+  "New Tab"="^t";
+}'
