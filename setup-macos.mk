a7a174c5303606b3ac1460a9a4b1ab5b644438fa
Eldar Iskhakov <iskhakov.e11@gmail.com>
2021-11-12 17:19:43 +0300

    update brew

--- setup-macos.mk
+++ setup-macos.mk
@@ -1,8 +1,9 @@
 install-brew:
+	xcode-select --install
 	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 install-brew-packages:
 	brew bundle --file macos/Brewfile
 
-set-settings:
+macos-set-settings:
 	sh macos/settings.sh
