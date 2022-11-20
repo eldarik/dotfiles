3fddf61aec49619a05c0073c6896b85cdc34a911
Eldar Iskhakov <iskhakov.e11@gmail.com>
2022-11-20 22:32:25 +0400

    update ubuntu setup

--- setup-ubuntu.mk
+++ setup-ubuntu.mk
@@ -31,26 +31,13 @@ install-apt-packages:
 			pkg-config \
 			ripgrep
 
-install-docker:
-	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-	sudo apt update -y -qq
-	sudo apt install -y -qq docker-ce
-	sudo groupadd docker
-	sudo usermod -aG docker eldar
-
 install-docker-compose:
 	sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 	sudo chmod +x /usr/local/bin/docker-compose
 
 install-ubuntu-nvim:
-	- mkdir ~/.config
-	- mkdir ~/.config/nvim/
-	- mkdir ~/.local/
-	- wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz
-	- tar -xvf nvim-linux64.tar.gz
-	- mv nvim-linux64 ~/.local/nvim
-	- mv nvim-linux64 ~/.local/nvim
+	wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.deb
+	sudo dpkg -i nvim-linux64.deb
 
 install-bat:
 	wget https://github.com/sharkdp/bat/releases/download/v0.12.1/bat_0.12.1_amd64.deb
